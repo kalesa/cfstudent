b3057920f0ff8892eceae0c442b9124f7f3d4830
Marissa Abao <iza.abao@gmail.com>
2016-06-04 20:52:12 +0800

    improved products pages

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -52,7 +52,6 @@ class ProductsController < ApplicationController
   end
 
   # DELETE /products/1
-  # DELETE /products/1.json
   def destroy
     @product.destroy
     respond_to do |format|
@@ -69,6 +68,6 @@ class ProductsController < ApplicationController
 
     # Never trust parameters from the scary internet, only allow the white list through.
     def product_params
-      params.require(:product).permit(:name, :description, :image_url)
+      params.require(:product).permit(:name, :description, :image_url, :color)
     end
 end
