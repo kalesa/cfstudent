71bb99be150e9773eecf7d40926702c7070653e4
Marissa Abao <iza.abao@gmail.com>
2016-06-06 15:30:26 +0800

    db migrate

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160604080322) do
+ActiveRecord::Schema.define(version: 20160605120052) do
 
   create_table "object_names", force: :cascade do |t|
     t.datetime "created_at", null: false
@@ -25,6 +25,7 @@ ActiveRecord::Schema.define(version: 20160604080322) do
     t.datetime "created_at",  null: false
     t.datetime "updated_at",  null: false
     t.string   "color"
+    t.string   "price"
   end
 
 end
