fdd9d67c257aab7e7b0946310597c7c04cad46b9
Marissa Abao <iza.abao@gmail.com>
2016-06-19 17:10:52 +0800

    routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,11 @@
 Rails.application.routes.draw do
   resources :products
-  get 'about', to: 'static_pages#about'
-  get 'contact', to: 'static_pages#contact'
+  #get 'about', to: 'static_pages#about'
+  #get 'contact', to: 'static_pages#contact'
+  get 'static_pages/about'
+  get 'static_pages/contact'
   get 'static_pages/index'
+  #get 'static_pages/landing_page'
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
