03d1f6544a211725346dcab97b2acb61f2f1034d
Marissa Abao <iza.abao@gmail.com>
2016-07-04 21:43:38 +0800

    5.11 added welcome message

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -2,9 +2,16 @@ class UserMailer < ApplicationMailer
   default from: "iza.abao@gmail.com"
 
   def contact_form(email, name, message)
-  @message = message
+    @message = message
     mail(:from => email,
         :to => 'iza.abao@gmail.com',
         :subject => "A new contact form message from #{name}")
+  end
+
+  def welcome(user)
+    @user = user
+    mail(:from => 'iza.abao@gmail.com',
+        :to => @user.email,
+        :subject => "Welcome to IzaBike")
   end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -4,4 +4,13 @@ class User < ActiveRecord::Base
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
   has_many :orders
+
+  after_create :send_welcome_mail
+
+  private
+  
+  def send_welcome_mail
+    UserMailer.welcome(self).deliver_now
+  end
 end
+
