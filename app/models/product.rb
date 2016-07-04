3650050f212f4b22a9ca50f7572c6a55e38bc94a
Marissa Abao <iza.abao@gmail.com>
2016-07-04 21:59:11 +0800

    6.2 fixed product_spec.rb

--- app/models/product.rb
+++ app/models/product.rb
@@ -2,11 +2,10 @@ class Product < ActiveRecord::Base
   has_many :orders
   has_many :comments
 
+  validates :name, presence: true
+  validates :price, numericality: { only_decimal: true }
+
   def average_rating
     comments.average(:rating).to_f
   end
-
-  validates :name, presence: true
-  validates :price, numericality: { only_decimal: true }
-  
 end
--- spec/model/product_spec.rb
+++ spec/model/product_spec.rb
@@ -1,16 +1,18 @@
 require 'rails_helper'
   describe Product do
     before do
-      @product = Product.create!(name: "race bike")
+      @product = Product.create!(name: "race bike", price: 1000)
       @user = User.create!(email: "iza_abao@yahoo.com", password: "Maharlika0")
       @product.comments.create!(rating: 1, user: @user, body: "Stable at speed and over rough stuff")
       @product.comments.create!(rating: 3, user: @user, body: "Latest trail bike is one to watch")
       @product.comments.create!(rating: 5, user: @user, body: "Better brakes")
     end
-      it "returns the average rating of all comments" do
+
+    it "returns the average rating of all comments" do
+      expect(@product.average_rating).to eq(3)
     end
-      it "invalidates a product without a product name" do
-        expect(Product.new(description: " Nice bike ")).not_to be_valid
+
+    it "invalidates a product without a product name" do
+      expect(Product.new(description: " Nice bike ")).not_to be_valid
     end
-  end
 end
