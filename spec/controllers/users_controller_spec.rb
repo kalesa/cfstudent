2332393e008f2d3e535badb9e41ea126cc167a68
Marissa Abao <iza.abao@gmail.com>
2016-07-06 08:07:45 +0800

    6.3 modified users_controller_spec.rb

--- spec/controllers/users_controller_spec.rb
+++ spec/controllers/users_controller_spec.rb
@@ -1,31 +1,29 @@
 require 'rails_helper'
 
 describe UsersController, :type => :controller do
-  context "GET #show" do
-    before do
-      sign_in user
+  before do
+    @user = User.create!(email: "iza_abao@yahoo.com", password: "Maharlika0")
+  end
 
-    it "Loads correct user details"
-      get :show, id: current_user.id
-      expect(response).to be_success
-      expect(response).to have_http_status(200)
-      expect(assigns(:user)).to eq user
+  describe "#show" do
+    context "User is logged in" do
+      before do
+        sign_in @user
+      end
+
+      it "Loads correct user details" do
+        get :show, id: @user.id
+        expect(response).to be_success
+        expect(response).to have_http_status(200)
+        expect(assigns(:user)).to eq @user
+      end
     end
+
+    context "No user is logged in" do
+      it "redirects to login" do
+         get :show, id: @user.id
+         expect(response).to redirect_to(new_user_session_path)
+      end
+    end    
   end
-
-  let(:user) { User.create!(email: "iza_abao@yahoo.com", password: "Maharlika0") }
-
-  describe "GET #show" do
-     context "User is logged in" do
-
-     end
-
-     context "No user is logged in" do
-       it "redirects to login" do
-         get :show, id: user.id
-         expect(response).to redirect_to(root_path)
-       end
-     end
-  end
-
 end
