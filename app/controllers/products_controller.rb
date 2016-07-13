e4b723a134e05a69940fdaeeb4745e6f22980da7
Marissa Abao <iza.abao@gmail.com>
2016-07-13 09:44:18 +0800

    6.9 added logger.debug to products_controller.rb

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -12,6 +12,7 @@ class ProductsController < ApplicationController
     else
       @products = Product.all.paginate(:page => params[:page], :per_page => 10)
     end
+    logger.debug 'HELLO!! I AM A DEBUGGING STATEMENT.'
   end
 
   # GET /products/1
