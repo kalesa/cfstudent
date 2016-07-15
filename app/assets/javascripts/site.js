4924255b3de7beb67d488b81f1653fa83ab0fb28
Marissa Abao <iza.abao@gmail.com>
2016-07-15 19:32:31 +0800

    6.12 added id=add_review and modified raty

--- app/assets/javascripts/site.js
+++ app/assets/javascripts/site.js
@@ -1,14 +1,23 @@
-$(document).on('ready page:load', function(){
-    $('.rating').raty( { path: '/assets', scoreName: 'comment[rating]' });
-    $('.rated').raty({ path: '/assets',
-      readOnly: true,
-      score: function() {
-        return $(this).attr('data-score');
-      }
-    });
+$(document).on('ready page:load', function(){rating();
+    //$('.rating').raty( { path: '/assets', scoreName: 'comment[rating]' });
+    //$('.rated').raty({ path: '/assets',
+      //readOnly: true,
+      //score: function() {
+        //return $(this).attr('data-score');
+      //}
+    //});
     $('.img-zoom').elevateZoom({
       zoomType: "lens", 
       lensShape : "round", 
       lensSize: 200
     });
 });
+function rating() {
+  $('.rating').raty( { path: '/assets', scoreName: 'comment[rating]' });
+
+  $('.rated').raty({ path: '/assets',
+    readOnly: true,
+    score: function() {
+    return $(this).attr('data-score');}
+  });
+}
