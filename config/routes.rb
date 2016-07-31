8d94513623f1489d46ef14a4429d988a6c02c655
Marissa Abao <iza.abao@gmail.com>
2016-07-31 15:22:48 +0800

    added sort_paginate_ajax

--- config/routes.rb
+++ config/routes.rb
@@ -8,9 +8,10 @@ Rails.application.routes.draw do
   
   #get 'about', to: 'static_pages#about'
   #get 'contact', to: 'static_pages#contact'
+  get 'static_pages/index'
   get 'static_pages/about'
   get 'static_pages/contact'
-  get 'static_pages/index'
+  get 'static_pages/orders'
   
   post 'static_pages/thank_you'
 
