2b6f265847a577d1bd1f1c2a56d2148085267467
Marissa Abao <iza.abao@gmail.com>
2016-08-03 22:10:48 +0800

    6.14 added average_rating

--- app/jobs/comment_update_job.rb
+++ app/jobs/comment_update_job.rb
@@ -2,7 +2,7 @@ class CommentUpdateJob < ApplicationJob
   queue_as :default
 
   def perform(comment, current_user)
-  ProductChannel.broadcast_to comment.product_id, comment: render_comment(comment, comment.user)
+    ProductChannel.broadcast_to comment.product_id, comment: render_comment(comment, comment.user), average_rating: comment.product.average_rating
 end
 
 private
